6837f6f8daa219898f0e669a297ea91a0b3c1a6f
Ramandeep Bhagat <rdbhagat999@gmail.com>
2024-09-01 17:00:40 +0530

    updated code

--- src/dsa/dsa.go
+++ src/dsa/dsa.go
@@ -1,5 +1,67 @@
 package dsa
 
+import (
+	"errors"
+	"fmt"
+	"math"
+	"time"
+)
+
+type Stack struct {
+	// Write a program to implement a stack using an array.
+	data []int
+}
+
+func (s *Stack) Push(item int) {
+	s.data = append(s.data, item)
+}
+
+func (s *Stack) Pop() (int, error) {
+	if len(s.data) == 0 {
+		return 0, errors.New("Stack is empty")
+	}
+
+	item := s.data[len(s.data)-1]
+	s.data = s.data[0 : len(s.data)-1]
+
+	return item, nil
+}
+
+func IsPrime(n int) bool {
+	if n < 2 {
+		return false
+	}
+
+	for i := 2; i < int(math.Sqrt(float64(n))); i++ {
+		if n%i == 0 {
+			return false
+		}
+	}
+
+	return true
+}
+
+func FindMinMaxInArray(a []int) (int, int) {
+
+	if len(a) == 0 {
+		return 0, 0
+	}
+
+	min := a[0]
+	max := a[0]
+
+	for _, v := range a {
+		if v < min {
+			min = v
+		}
+		if v > max {
+			max = v
+		}
+	}
+
+	return min, max
+}
+
 func FibonacciRecursive(n int) int {
 	if n <= 1 {
 		return n
@@ -9,9 +71,24 @@ func FibonacciRecursive(n int) int {
 }
 
 func FactorialRecursive(n int) int {
-	if n == 1 {
+	if n == 0 || n == 1 {
 		return 1
 	}
 
 	return n * FactorialRecursive(n-1)
+}
+
+func sendMessageToChannel(ch chan<- string, msg string) {
+	time.Sleep(5 * time.Second)
+	ch <- msg
+}
+
+func CallGoRoutine(sendTextMsg string) {
+	ch := make(chan string)
+
+	go sendMessageToChannel(ch, sendTextMsg)
+
+	msg := <-ch
+
+	fmt.Println(msg)
 }
